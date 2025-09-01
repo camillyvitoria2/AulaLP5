d867cf25771cb64e701e0525c7cfaf5b56b9b4a9
camillyvitoria2 <camilly.souza2@estudante.ifms.edu.br>
2025-09-01 10:16:56 -0400

    Aula que criamos o habilitar.

---
+++ src/aulalp5/AulaLp5.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aulalp5;
+
+/**
+ *
+ * @author u04410827189
+ */
+public class AulaLp5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
